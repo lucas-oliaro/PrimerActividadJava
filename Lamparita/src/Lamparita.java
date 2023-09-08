26ac964b8066bbe2a8f0f302c9d880c742a8e37e
Loli <lucasoliaro@gmail.com>
2023-09-08 17:59:51 -0300

    Updated

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,39 @@
 public class Lamparita {
+    //Atributo de la clase
+
+    //defino atributo del estado
+    private boolean estado;
+
+    //defino un cosntructor para que el estado este sea apagado
+    //en java uns constructor no necesita el tipo que devueve porque tiene devovler de la misma clase que pertence
+
+    public Lamparita(){
+        this.estado = false;
+        //implicitamente devuelve return this
+    }
+    
+
+    public void encender(){
+        //Si es void procedimiento
+        // el this == self en python
+        this.estado = true;
+    }
+
+    public void apagar(){
+        this.estado = false;
+    }
+
+    //Funcion que devuelve el estado de mi lamparaita
+    public boolean estado(){
+        return estado;
+    }
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
+        //Aca inicia toto
+        
+
     }
-}
+        
+    }
+
